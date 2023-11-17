5dac15dba5848d5b8212af654d60f30fecc43585
Bartosz Snieg <snieg45@gmail.com>
2023-11-17 10:02:36 +0100

    Added simba::core::Result
    For optional return

--- application/application_no_ipc.h
+++ application/application_no_ipc.h
@@ -11,22 +11,37 @@
 #ifndef CORE_APPLICATION_APLICATION_NO_IPC_H_
 #define CORE_APPLICATION_APLICATION_NO_IPC_H_
 #include <memory>
+#include <nlohmann/json.hpp>
 #include <string>
 
 #include "application/Iapplication.h"
 #include "logger/ILogger.h"
 #include "logger/console_logger.h"
+
 namespace simba {
 namespace core {
 class ApplicationNoIPC : public IApplication {
  protected:
   std::shared_ptr<logger::ILogger> logger_;
-  virtual void Run(const std::unordered_map<std::string, Parm>& parms) override {
+  nlohmann::json data{};
+  virtual void Run(
+      const std::unordered_map<std::string, Parm>& parms) override {
     logger_->Error("Application function: Run is not implemented: ");
   }
 
   void onRun(const std::unordered_map<std::string, Parm>& parms) override {
     this->logger_ = std::make_shared<logger::ConsoleLogger>();
+    const auto config = parms.find("config_path");
+    if (config != parms.cend()) {
+      std::ifstream f(config->second.GetValue());
+      if (f.is_open()) {
+        data = nlohmann::json::parse(f);
+      } else {
+        logger_->Error("File on path: " + config->second.GetValue() +
+                       " not exist!!");
+      }
+    }
+
     logger_->Info("Application started");
     this->Run(parms);
     logger_->Info("Application stopped");
